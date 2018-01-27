68f8d26512cc7e945924981fd5fad02d01a870fe
David Andrews <dta@alumni.cmu.edu>
2018-01-27 16:50:10 -0500

    Add trees and change api

--- transmission/Assets/GameLogic.cs
+++ transmission/Assets/GameLogic.cs
@@ -6,18 +6,19 @@ public class GameLogic : MonoBehaviour {
 	public GameObject roadStraight;
 	public GameObject roadIntersectionT;
 	public GameObject roadTurn;
+	public GameObject[] environmentTrees;
 
 	// Use this for initialization
 	void Start () {
 		roadStraight.transform.localScale = new Vector3 (1, 1, -1);
 		roadIntersectionT.transform.localScale = new Vector3 (1, 1, -1);
-		instantiateRoadStraight (0, 0, Direction.NORTH);
-		instantiateRoadTurnLeft (0, 1, Direction.NORTH);
-		instantiateRoadTurnRight (-1, 1, Direction.WEST);
-		instantiateRoadStraight (-1, 2, Direction.NORTH);
-		instantiateRoadTurnRight (-1, 3, Direction.NORTH);
-		instantiateRoadTurnLeft (0, 3, Direction.EAST);
-		instantiateRoadIntersectionT (0, 4, Direction.NORTH);
+		instantiateRoad (Road.STRAIGHT, 0, 0, Direction.NORTH);
+		instantiateRoad (Road.TURN_LEFT, 0, 1, Direction.NORTH);
+		instantiateRoad (Road.TURN_RIGHT, -1, 1, Direction.WEST);
+		instantiateRoad (Road.BRANCH_LEFT, -1, 2, Direction.NORTH);
+		instantiateRoad (Road.TURN_RIGHT, -1, 3, Direction.NORTH);
+		instantiateRoad (Road.TURN_LEFT, 0, 3, Direction.EAST);
+		instantiateRoad (Road.INTERSECTION_T, 0, 4, Direction.NORTH);
 	}
 	
 	// Update is called once per frame
@@ -25,24 +26,27 @@ public class GameLogic : MonoBehaviour {
 		
 	}
 
-	void instantiateRoad(GameObject roadType, int x, int z, int rotation) {
-		Instantiate (roadType, new Vector3(2 * x, 0, 2 * z), Quaternion.Euler(new Vector3(0, rotation * 90, 0)));
+	private GameObject instantiateRoad(Road roadType, int x, int z, Direction heading) {
+		switch (roadType) {
+		case Road.STRAIGHT:
+			return instantiateRoad (roadStraight, x, z, (int) heading);
+		case Road.TURN_LEFT:
+			return instantiateRoad (roadTurn, x, z, (int) heading - 1);
+		case Road.TURN_RIGHT:
+			return instantiateRoad (roadTurn, x, z, (int) heading - 2);
+		case Road.BRANCH_LEFT:
+			return instantiateRoad (roadIntersectionT, x, z, (int) heading);
+		case Road.BRANCH_RIGHT:
+			return instantiateRoad (roadIntersectionT, x, z, (int) heading - 2);
+		case Road.INTERSECTION_T:
+			return instantiateRoad (roadIntersectionT, x, z, (int) heading - 1);
+		default:
+			return null;
+		}
 	}
 
-	void instantiateRoadStraight(int x, int z, Direction dir) {
-		instantiateRoad(roadStraight, x, z, (int) dir);
-	}
-
-	void instantiateRoadIntersectionT(int x, int z, Direction dir) {
-		instantiateRoad(roadIntersectionT, x, z, (int) dir - 1);
-	}
-
-	void instantiateRoadTurnLeft(int x, int z, Direction dir) {
-		instantiateRoad(roadTurn, x, z, (int) dir);
-	}
-
-	void instantiateRoadTurnRight(int x, int z, Direction dir) {
-		instantiateRoad(roadTurn, x, z, (int) dir - 1);
+	private GameObject instantiateRoad(GameObject roadType, int x, int z, int rotation) {
+		return Instantiate (roadType, new Vector3(2 * x, 0, 2 * z), Quaternion.Euler(new Vector3(0, rotation * 90, 0)));
 	}
 }
 
@@ -52,55 +56,59 @@ enum Direction {
 
 enum Side {Right, Left};
 
-public class Rules {
+enum Road {
+	STRAIGHT, TURN_LEFT, TURN_RIGHT, INTERSECTION_T, BRANCH_LEFT, BRANCH_RIGHT
+}
+	
+struct Position {
+	public int roadSection;
+	public Side side;
 
-	private struct Position {
-		public int roadSection;
-		public Side side;
-
-		public Position (int roadSection, Side side) {
-			this.roadSection = roadSection;
-			this.side = side;
-		}
+	public Position (int roadSection, Side side) {
+		this.roadSection = roadSection;
+		this.side = side;
 	}
+}
+	
+class EnvironmentObject {
+	public Position pos;
 
-	private class EnvironmentObject {
-		public Position pos;
-
-		public EnvironmentObject(Position pos) {
-			this.pos = pos;
-		}
+	public EnvironmentObject(Position pos) {
+		this.pos = pos;
 	}
+}
+	
+class Tree : EnvironmentObject {
+	public Color color;
 
-	private class Tree : EnvironmentObject {
-		public Color color;
-
-		public Tree(Position pos, Color color) : base(pos) {
-			this.color = color;
-		}
+	public Tree(Position pos, Color color) : base(pos) {
+		this.color = color;
 	}
+}
+	
+class Sign : EnvironmentObject {
+	public Color color;
 
-	private class Sign : EnvironmentObject {
-		public Color color;
-
-		public Sign(Position pos, Color color) : base(pos) {
-			this.color = color;
-		}
+	public Sign(Position pos, Color color) : base(pos) {
+		this.color = color;
 	}
+}
+	
+struct Level {
+	public string rules;
+	public Side correctTurn;
+	public int roadLength;
+	public EnvironmentObject[] environmentObjects;
 
-	private struct Level {
-		public string rules;
-		public Side correctTurn;
-		public int roadLength;
-		public EnvironmentObject[] environmentObjects;
-
-		public Level(string rules, Side correctTurn, int roadLength, EnvironmentObject[] environmentObjects) {
-			this.environmentObjects = environmentObjects;
-			this.correctTurn = correctTurn;
-			this.rules = rules;
-			this.roadLength = roadLength;
-		}
+	public Level(string rules, Side correctTurn, int roadLength, EnvironmentObject[] environmentObjects) {
+		this.environmentObjects = environmentObjects;
+		this.correctTurn = correctTurn;
+		this.rules = rules;
+		this.roadLength = roadLength;
 	}
+}
+
+public class Levels {
 
 	private Level level1 = new Level(
 		"If one side of the road has more yellow trees, turn to that side",
